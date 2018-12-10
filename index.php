a500d437e4e180b575ae7b54b8c9ffdfd6b63e24
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2018-12-10 09:35:03 -0600

    Welcome to UAQ-ANDA

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 
-echo "hola";
+echo "Servidor dedicado a mantener el código de UAQ-ANDA <br> Para acceder al código contacte a los desarrolladores en la Facultad de Infomática.
+      Para tener acceso al sistema, ingrese mediante los sistemas dedicados.";
  ?>

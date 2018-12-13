7bcc5ce673ff1090cad07366ae99be1e43a789cd
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2018-12-12 19:22:03 -0600

    aaa

--- UAQ-ANDA/views/Administrador/retroalimentaciones.php
+++ UAQ-ANDA/views/Administrador/retroalimentaciones.php
@@ -5,24 +5,26 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <script type="text/javascript" src="<?=JS?>preHash.js"></script>
 
-  <!--Este header contiene el botón para regresar al menú principal-->
-  <header>
-      <button class="casa" onclick="menu()"><img class="icono" src="<?=IMG?>school-bus.png"></button>
-      <h1>UAQ-ANDA</h1>
-      <p>Retroalimentaciones</p>
-    </header>
+  <body>
+    <!--Este header contiene el botón para regresar al menú principal-->
+    <header>
+        <button class="casa" onclick="menu()"><img class="icono" src="<?=IMG?>school-bus.png"></button>
+        <h1>UAQ-ANDA</h1>
+        <p>Retroalimentaciones</p>
+      </header>
 
-    <!--Aquí está el buscador que sirve como filtro para las retroalimentaciones-->
-    <div class="datos">
+      <!--Aquí está el buscador que sirve como filtro para las retroalimentaciones-->
       <div class="datos">
-        <h2>Fecha:</h2>
-        <input type="date" name="" id="date">
-        <button class="search" onclick="cali()" >Buscar</button>
+        <div class="datos">
+          <h2>Fecha:</h2>
+          <input type="date" name="" id="date">
+          <button class="search" onclick="cali()" >Buscar</button>
+        </div>
       </div>
-    </div>
 
 
-    <div id="infoC"></div>
+      <div id="infoC"></div>
+  </body>
 
 <script type="text/javascript" src="<?=JS?>funciones.js"></script>
 <script type="text/javascript" src="<?=JS?>config.js"></script>

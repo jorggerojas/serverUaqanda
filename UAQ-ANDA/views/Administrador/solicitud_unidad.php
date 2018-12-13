65ef05e8009d81c88d2e7fb9ba0b9e49de54f3be
jdavidlo <jdibicho@gmail.com>
2018-12-12 23:48:38 -0600

    quiero dormir
    quiero dormir

--- UAQ-ANDA/views/Administrador/solicitud_unidad.php
+++ UAQ-ANDA/views/Administrador/solicitud_unidad.php
@@ -5,17 +5,23 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <body onload="mostrarSolicitudes();">
+
+	<!--En el header se encuentra el boton que dirige al menu principal y cerrar sesion-->
 	<header>
 		<button class="casa" onclick="menu()">	<img class="icono" src="<?=IMG?>school-bus.png"></button>
 		<h1>UAQ-ANDA</h1>
 		<button class="cerrar" onclick="modalSesion();"><img class="icono2" src="<?=IMG?>exit.png"></button>
 	</header>
 
+	<!--Muestra cuantas solicitudes hay pendientes-->
 	<div class="solicitud">
 		<h2 id="num"></h2><h2> Solicitudes de unidad pendientes</h2>
 	</div>
+
+	<!--Se muestran los datos-->
 	<div id="solicitudes"></div>
 
+	<!--Estos divs se usan para el modal-->
 	<div class="fondo"></div>
 	<div class="mensaje"></div>
 	<script type="text/javascript" src="<?=JS?>funciones.js"></script>

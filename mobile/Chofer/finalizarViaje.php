73f60d0733906382aeb29fe8b188c819cba4c191
Donovan20 <43098688+Donovan20@users.noreply.github.com>
2018-12-12 23:21:50 -0600

    vientos

--- mobile/Chofer/finalizarViaje.php
+++ mobile/Chofer/finalizarViaje.php
@@ -36,13 +36,14 @@ function finalizar($idViaje,$inicio,$final){
     'body' => '¡El viaje a finalizado con exito!'
   );
 
-  $ch = curl_init();
+  
   $server_key= 'AAAA38EOB9k:APA91bEjbHv0MKUUv45NhyAIxVmQwWm0vN8ScAetaMJhZhgQuHFZljdiZ8w02rn-73r0NEwK83EhWb-qqFpaHlEGNXcejyK7ovzJWjg-jep0YXFcNVVFrHvawKMLiPddwB6qsnmpThTP';
   $headers = array(
     'Content-Type:application/json',
     'Authorization:key='.$server_key
   );
   for ($i=0; $i <count($datos); $i++) {
+    $ch = curl_init();
     $tok = $datos[$i]['token'];
     $fields = array
     (
@@ -55,9 +56,9 @@ function finalizar($idViaje,$inicio,$final){
     curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
     curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
     curl_setopt( $ch,CURLOPT_POSTFIELDS,json_encode($fields));
+    $result = curl_exec($ch);
+    curl_close( $ch );
   }
-  $result = curl_exec($ch);
-  curl_close( $ch );
 
   return $bandera;
 }

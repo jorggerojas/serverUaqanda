fd7bdc697fbd2cc6f32c715210e3f4ec54473f12
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2019-04-06 22:27:26 -0600

    Update confirmar.php

--- mobile/confirmar.php
+++ mobile/confirmar.php
@@ -2,8 +2,17 @@
 include_once('conexion.php');
 $db = new Conexion();
 
-$u =  $db->update("UPDATE reservaciones SET Status='Confirmado'");
-echo json_encode(1);
+/*$u =  $db->update("UPDATE reservaciones SET Status='Confirmado'");
+echo json_encode(1);*/
+
+        $datos = $db->query("SELECT * FROM usuarios WHERE ClaveExpediente='".$_POST['pass']."');
+        if($datos != ""){
+            //Se envían los datos en un arreglo JSON
+            echo json_encode($datos);
+        }
+        else{
+            echo json_encode("2");
+        }
 
 
 ?>
